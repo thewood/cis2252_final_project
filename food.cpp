8df2fc53281c840dbc0430ffb1ee3c93c8dd66fe
t.j.e <tj@elkion.com>
2014-08-06 17:50:38 -0400

    adding food class

--- food.cpp
+++ food.cpp
@@ -1,15 +1,9 @@
 //
-//  main.cpp
+//  food.cpp
 //  cis2252_final_project
 //
 //  Created by thomas j. elward on 8/6/14.
 //  Copyright (c) 2014 tje. All rights reserved.
 //
 
-#include <iostream>
-
-int main(int argc, const char * argv[])
-{
-
-}
-
+#include "food.h"
---
+++ food.h
@@ -0,0 +1,14 @@
+//
+//  food.h
+//  cis2252_final_project
+//
+//  Created by thomas j. elward on 8/6/14.
+//  Copyright (c) 2014 tje. All rights reserved.
+//
+
+#ifndef __cis2252_final_project__food__
+#define __cis2252_final_project__food__
+
+#include <iostream>
+
+#endif /* defined(__cis2252_final_project__food__) */
