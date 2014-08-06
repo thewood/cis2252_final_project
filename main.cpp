58ef7946141da89d401f74150a58afb32b8cd0c7
t.j.e <tj@elkion.com>
2014-08-06 17:34:14 -0400

    test
    test

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cis2252_final_project
+//
+//  Created by thomas j. elward on 8/6/14.
+//  Copyright (c) 2014 tje. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
